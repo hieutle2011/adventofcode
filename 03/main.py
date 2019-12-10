d56d213d1fa994952d00f1ebac66593fdee358ba
dthanhoanh <dthanhoanh@tma.com.vn>
2019-12-10 18:03:48 +0700

    done first half of day 3

---
+++ 03/main.py
@@ -0,0 +1,136 @@
+import os
+import copy
+
+
+class Point:
+    def __init__(self, x_ord, y_ord):
+        self.x = x_ord
+        self.y = y_ord
+
+    def get_manhatan_distance(self, point):
+        return abs(self.x - point.x) + abs(self.y - point.y)
+
+    def get_manhatan_distance_from_central(self):
+        return abs(self.x) + abs(self.y)
+
+    def isExist(self):
+        if self.x == None or self.y == None:
+            return False
+        else:
+            return True
+
+    def nextPoint(self, command):
+        x = 0
+        y = 0
+        code = command[0]
+        step = int(command[1:])
+        if code == 'R':
+            x += step
+        elif code == 'L':
+            x -= step
+        elif code == 'U':
+            y += step
+        elif code == 'D':
+            y -= step
+        return Point(self.x + x, self.y + y)
+
+
+class Line:
+    def __init__(self, ptA, ptB):
+        min_x = 0
+        min_y = 0
+        max_x = 0
+        max_y = 0
+        if ptA.x == ptB.x:
+            min_x = ptA.x
+            max_x = ptA.x
+            self.direction = 'VERTICAL'
+            if ptA.y > ptB.y:
+                min_y = ptB.y
+                max_y = ptA.y
+            else:
+                min_y = ptA.y
+                max_y = ptB.y
+        elif ptA.y == ptB.y:
+            min_y = ptA.y
+            max_y = ptA.y
+            self.direction = 'HORIZONTAL'
+            if ptA.x > ptB.x:
+                min_x = ptB.x
+                max_x = ptA.x
+            else:
+                min_x = ptA.x
+                max_x = ptB.x
+        else:
+            self.direction = 'DIAGONAL'
+
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+
+    def cut_with(self, line):
+        xP = None
+        yP = None
+
+        if self.direction != line.direction:
+            if self.direction == 'VERTICAL':
+                if self.min_x >= line.min_x and self.min_x <= line.max_x and line.min_y >= self.min_y and line.min_y <= self.max_y:
+                    xP = self.min_x
+                    yP = line.min_y
+            elif self.direction == 'HORIZONTAL':
+                if line.min_x >= self.min_x and line.min_x <= self.max_x and self.min_y >= line.min_y and self.min_y <= line.max_y:
+                    xP = line.min_x
+                    yP = self.min_y
+
+        return Point(xP, yP)
+
+
+def parseInputFile(path):
+    file = open(path)
+    wires = []
+    for line in file:
+        wire = line.replace('\n', '').split(',')
+        wires.append(wire)
+    return wires
+
+
+def main():
+    path = os.getcwd() + '/input.txt'
+    wires = parseInputFile(path)
+
+    centralPt = Point(0, 0)
+    lines = []
+
+    for wire in wires:
+        sub_lines = []
+        currentPt = copy.deepcopy(centralPt)
+        for cmd in wire:
+            nextPt = currentPt.nextPoint(cmd)
+            line = Line(currentPt, nextPt)
+            sub_lines.append(line)
+            currentPt = nextPt
+        lines.append(sub_lines)
+    
+    valid_points = []
+
+    for line_0 in lines[0]:
+        for line_1 in lines[1]:
+            p = line_0.cut_with(line_1)
+            if p.isExist():
+                valid_points.append(p)
+
+    print('Number of cut points: {}'.format(len(valid_points)))
+
+    min_distance = 1000000 # abitrary big number
+    for p in valid_points:
+        distance = p.get_manhatan_distance_from_central()
+        if distance != 0 and distance < min_distance:
+            # print('====distance {}, x {}, y {}'.format(distance, p.x, p.y))
+            min_distance = distance
+    
+    print('Distance to closet point: {}'.format(min_distance))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 03/test.py
@@ -0,0 +1,83 @@
+import unittest
+from main import Point, Line
+
+
+class TestPointMethods(unittest.TestCase):
+    def test_new_point(self):
+        new = Point(3, 3)
+        self.assertEqual(new.x, 3)
+        self.assertEqual(new.y, 3)
+        self.assertEqual(new.isExist(), True)
+
+    def test_new_point_null(self):
+        new = Point(None, 3)
+        self.assertEqual(new.x, None)
+        self.assertEqual(new.y, 3)
+        self.assertEqual(new.isExist(), False)
+
+    def test_distance_from_central(self):
+        new = Point(-3, -3)
+        self.assertIs(type(new.x), int)
+        self.assertEqual(new.get_manhatan_distance_from_central(), 6)
+
+    def test_distance_from_point(self):
+        pA = Point(1, 1)
+        pB = Point(3, 3)
+        self.assertEqual(pA.get_manhatan_distance(pB), 4)
+
+    def test_nextPoint_with_0_0(self):
+        centralPoint = Point(0, 0)
+        nxPoint = centralPoint.nextPoint('R8')
+        self.assertEqual(nxPoint.x, 8)
+        self.assertEqual(nxPoint.y, 0)
+
+        nxPoint = centralPoint.nextPoint('U5')
+        self.assertEqual(nxPoint.x, 0)
+        self.assertEqual(nxPoint.y, 5)
+
+        nxPoint = centralPoint.nextPoint('L8')
+        self.assertEqual(nxPoint.x, -8)
+        self.assertEqual(nxPoint.y, 0)
+
+        nxPoint = centralPoint.nextPoint('D5')
+        self.assertEqual(nxPoint.x, 0)
+        self.assertEqual(nxPoint.y, -5)
+
+    def test_nextPoint_with_custom_point(self):
+        nxPoint = Point(8, 0).nextPoint('R1')
+        self.assertEqual(nxPoint.x, 9)
+        self.assertEqual(nxPoint.y, 0)
+
+class TestLineMethods(unittest.TestCase):
+    def test_new_line_horizontal(self):
+        line = Line(Point(3,5), Point(8,5))
+        self.assertEqual(line.direction, 'HORIZONTAL')
+        self.assertEqual(line.min_x, 3)
+        self.assertEqual(line.max_x, 8)
+        self.assertEqual(line.min_y, 5)
+        self.assertEqual(line.max_y, 5)
+        
+    def test_new_line_vertical(self):
+        line = Line(Point(10,3), Point(10,10))
+        self.assertEqual(line.direction, 'VERTICAL')
+        self.assertEqual(line.min_x, 10)
+        self.assertEqual(line.max_x, 10)
+        self.assertEqual(line.min_y, 3)
+        self.assertEqual(line.max_y, 10)
+
+    def test_un_cut_lines(self):
+        hor = Line(Point(3,5), Point(8,5))
+        ver = Line(Point(10,3), Point(10,10))
+        cut_point = hor.cut_with(ver)
+        self.assertEqual(cut_point.isExist(), False)
+
+    def test_cut_lines(self):
+        hor = Line(Point(3,5), Point(8,5))
+        ver = Line(Point(6,3), Point(6,10))
+        cut_point = hor.cut_with(ver)
+        self.assertEqual(cut_point.isExist(), True)
+        self.assertEqual(cut_point.x, 6)
+        self.assertEqual(cut_point.y, 5)
+
+if __name__ == '__main__':
+    unittest.main()
