1b186bec9625a8d9b45931973c4c173b400ba94a
hieutle2011 <hieuletrung102@gmail.com>
2019-12-13 08:30:36 +0700

    wip day 5

--- 05/main.py
+++ 05/main.py
@@ -8,29 +8,55 @@ def parseInputFile(path):
         return [int(i) for i in line.split(',')]
 
 
-def getOpCodeIndex(array_length):
-    return [int(i) for i in range(0, array_length, 4)]
+def handleIO():
+    pass
 
 
 def handleOperation(opCode, opCodeIndex, progCodes):
+    # param opCode: string '1002'
+    # param opCodeIndex: int
+    # param progCodes: list
+    
+    # CBADE
+    #  1002,4,3,4
     indexA = opCodeIndex+1
     indexB = opCodeIndex+2
     indexResult = opCodeIndex+3
     ptrA = progCodes[indexA]
     ptrB = progCodes[indexB]
     ptrResult = progCodes[indexResult]
-    a = progCodes[ptrA]
-    b = progCodes[ptrB]
-    if opCode == PLUS_CODE:
-        progCodes[ptrResult] = a + b
-    elif opCode == MULTIPLY_CODE:
-        progCodes[ptrResult] = a * b
-    return progCodes
+
+    de = int(opCode[-2:]) # opcode: 1 or 2
+    a_mode, b_mode, c_mode = int(opCode[-3]), int(opCode[-4]), POSITION_MODE
+    # a, b, c = progCodes[indexA], progCodes[indexB], progCodes[indexResult]
+    
+    # value of first argument
+    if a_mode == POSITION_MODE:
+        a_val = progCodes[ptrA]
+    elif a_mode == IMMEDIATE_MODE:
+        a_val = ptrA
+
+    # value of second argument
+    if b_mode == POSITION_MODE:
+        b_val = progCodes[ptrB]
+    elif b_mode == IMMEDIATE_MODE:
+        b_val = ptrB
+
+    # where to write result to
+    if c_mode == POSITION_MODE: # always true
+        if de == PLUS_CODE:
+            progCodes[ptrResult] = a_val + b_val
+        elif de == MULTIPLY_CODE:
+            progCodes[ptrResult] = a_val * b_val
+        return progCodes
 
 
 PLUS_CODE = 1
 MULTIPLY_CODE = 2
 HALT_CODE = 99
+
+POSITION_MODE = 0
+IMMEDIATE_MODE = 1
 
 
 def main():
@@ -38,29 +64,29 @@ def main():
     program_codes = parseInputFile(path)
     EXPECT = 19690720
 
-    for noun in range(0, 100):
-        for verb in range(0, 100):
-            # reload program codes into memory
-            memory = copy.deepcopy(program_codes)
+    # for noun in range(0, 100):
+    #     for verb in range(0, 100):
+    #         # reload program codes into memory
+    #         memory = copy.deepcopy(program_codes)
 
-            # restore the gravity assist program
-            memory[1] = noun
-            memory[2] = verb
+    #         # restore the gravity assist program
+    #         memory[1] = noun
+    #         memory[2] = verb
 
-            opCodeIndexes = getOpCodeIndex(len(memory))
-            for index in opCodeIndexes:
-                opCode = memory[index]
-                if opCode == HALT_CODE:
-                    break
-                elif opCode == PLUS_CODE or opCode == MULTIPLY_CODE:
-                    memory = handleOperation(opCode, index, memory)
-                else:
-                    pass
+    #         opCodeIndexes = getOpCodeIndex(len(memory))
+    #         for index in opCodeIndexes:
+    #             opCode = memory[index]
+    #             if opCode == HALT_CODE:
+    #                 break
+    #             elif opCode == PLUS_CODE or opCode == MULTIPLY_CODE:
+    #                 memory = handleOperation(opCode, index, memory)
+    #             else:
+    #                 pass
 
-            if memory[0] == EXPECT:
-                print(noun, verb)
-                print(memory[0])
-                exit() # stop program immedietly
+    #         if memory[0] == EXPECT:
+    #             print(noun, verb)
+    #             print(memory[0])
+    #             exit() # stop program immedietly
 
 
 if __name__ == "__main__":
